c29ff461a2c9bf7fbd9c4817bc15ee9d4b45d036
harveywwu <harvey_wwu@hotmail.com>
2016-05-06 18:33:45 +0800

    Update bktest_aberration.py

--- bktest_aberration.py
+++ bktest_aberration.py
@@ -19,18 +19,29 @@ def aberration_sim( mdf, config):
     close_daily = config['close_daily']
     win = bbands[0]
     k = bbands[1]
+    chan = config.get('chan', 0)
+    if chan > 0:
+        chan_func = config['chan_func']
     tcost = config['trans_cost']
     unit = config['unit']
     freq = config['freq']
+    mode = config.get('mode', 'close')
     xdf = dh.conv_ohlc_freq(mdf, freq)
     boll_ma = dh.MA(xdf, win).shift(1)
     boll_std = dh.STDDEV(xdf, win).shift(1)
     upbnd = np.ceil((boll_ma + boll_std * k)/tick_base) * tick_base
     lowbnd = np.floor((boll_ma - boll_std * k)/tick_base) * tick_base
     boll_ma = np.floor(boll_ma/tick_base + 0.5) * tick_base
-    xdata = pd.concat([boll_ma, boll_std, upbnd, lowbnd], axis=1, keys=['ma', 'stdev', 'upbnd', 'lowbnd']).fillna(0)
-    mdf = mdf.join(xdata, how = 'left').fillna(method='ffill')
-    mdf['nxtopen'] = mdf['open'].shift(-1)
+    xdata = [boll_ma, boll_std, upbnd, lowbnd]
+    xkeys = ['ma', 'stdev', 'upbnd', 'lowbnd']
+    if chan > 0:
+        chan_h = eval(chan_func['high']['func'])(xdf, chan, **chan_func['high']['args'])
+        chan_l = eval(chan_func['low']['func'])(xdf, chan, **chan_func['low']['args'])
+        xdata = xdata + [ chan_h, chan_l ]
+        xkeys = xkeys + ['chan_h', 'chan_l']
+    xdf = pd.concat( xdata, axis=1, keys=xkeys).fillna(0)
+    mdf = mdf.join(xdf, how = 'left').fillna(method='ffill')
+    # mdf['next_open'] = mdf['open'].shift(-1)
     ll = mdf.shape[0]
     mdf['pos'] = pd.Series([0]*ll, index = mdf.index)
     mdf['cost'] = pd.Series([0]*ll, index = mdf.index)
@@ -43,10 +54,7 @@ def aberration_sim( mdf, config):
         mslice = mdf.ix[dd]
         min_id = mslice.min_id
         d = mslice.date
-        if len(curr_pos) > 1:
-            print "wrong position numbers", len(curr_pos)
-            continue
-        elif len(curr_pos) == 0:
+        if len(curr_pos) == 0:
             pos = 0
             start_pos = 0
         else:
@@ -54,6 +62,8 @@ def aberration_sim( mdf, config):
             start_pos = pos
         mdf.ix[dd, 'pos'] = pos
         if mslice.ma == 0:
+            continue
+        if chan > 0 and mslice.chan_h == 0:
             continue
         if min_id >=config['exit_min']:
             if (pos!=0) and ((d == end_d) or close_daily):
@@ -65,11 +75,19 @@ def aberration_sim( mdf, config):
                 mdf.ix[dd, 'cost'] -=  abs(pos) * (offset + mslice.close*tcost)
                 pos = 0
         else:
-            if ((mslice.high >= mslice.ma) and (pos<0)) or ((mslice.low <= mslice.ma) and (pos>0)):
+            if mode == 'close':
+                high_trig = mslice.close
+                low_trig  = mslice.close
+                trade_price = mslice.close
+            else:
+                high_trig = mslice.high
+                low_trig = mslice.low
+                trade_price = mslice.open
+            if ((high_trig >= mslice.ma) and (pos<0)) or ((low_trig <= mslice.ma) and (pos>0)):
                 if pos < 0: 
-                    exec_price = max(mslice.open, mslice.ma)
+                    exec_price = max(trade_price, mslice.ma)
                 else:
-                    exec_price = min(mslice.open, mslice.ma)
+                    exec_price = min(trade_price, mslice.ma)
                 curr_pos[0].close(exec_price - misc.sign(pos) * offset, dd)
                 tradeid += 1
                 curr_pos[0].exit_tradeid = tradeid
@@ -77,28 +95,25 @@ def aberration_sim( mdf, config):
                 curr_pos = []
                 mdf.ix[dd, 'cost'] -= abs(pos) * (offset + exec_price*tcost)
                 pos = 0
-            if ((mslice.high > mslice.upbnd) and (pos<=0)) or ((mslice.low < mslice.lowbnd) and (pos>=0)):
-                if (pos == 0):
-                    target_pos = (mslice.high > mslice.upbnd) * unit - (mslice.low < mslice.lowbnd) * unit
-                    if target_pos == 0:
-                        print "the min bar hit both up and low bounds, need to think about how to handle it", start_pos, mslice
-                        if mslice.close > mslice.ma:
-                            target_pos = (mslice.high > mslice.upbnd) * unit
-                            target = max(mslice.open, mslice.upbnd)
-                        else:
-                            trade_pos = - (mslice.low < mslice.lowbnd) * unit
-                            target =  min(mslice.open, mslice.lowbnd)
+            if ((high_trig > mslice.upbnd) and (pos<=0)) or ((low_trig < mslice.lowbnd) and (pos>=0)):
+                target_pos = ( high_trig > mslice.upbnd) * unit - (low_trig < mslice.lowbnd) * unit
+                if target_pos == 0:
+                    print "the min bar hit both up and low bounds, need to think about how to handle it", start_pos, mslice
+                    if mslice.close > mslice.ma:
+                        target_pos = (high_trig > mslice.upbnd) * unit
+                        target = max(trade_price, mslice.upbnd)
                     else:
-                        target = (mslice.high > mslice.upbnd) * max(mslice.open, mslice.upbnd) + ( mslice.low < mslice.lowbnd) * min(mslice.open, mslice.lowbnd)
-                    new_pos = pos_class([mslice.contract], [1], target_pos, target, mslice.ma, **pos_args)
-                    tradeid += 1
-                    new_pos.entry_tradeid = tradeid
-                    new_pos.open(target + misc.sign(target_pos)*offset, dd)
-                    curr_pos.append(new_pos)
-                    pos = target_pos
-                    mdf.ix[dd, 'cost'] -=  abs(target_pos) * (offset + target*tcost)
+                        trade_pos = - (low_trig < mslice.lowbnd) * unit
+                        target =  min(trade_price, mslice.lowbnd)
                 else:
-                    print "something wrong with position=%s, close =%s, MA=%s, upBnd=%s, lowBnd=%s" % ( pos, mslice.close, mslice.ma, mslice.upbnd, mslice.lowbnd)
+                    target = (high_trig > mslice.upbnd) * max(trade_price, mslice.upbnd) + ( low_trig < mslice.lowbnd ) * min(trade_price, mslice.lowbnd)
+                new_pos = pos_class([mslice.contract], [1], target_pos, target, target, **pos_args)
+                tradeid += 1
+                new_pos.entry_tradeid = tradeid
+                new_pos.open(target + misc.sign(target_pos)*offset, dd)
+                curr_pos.append(new_pos)
+                pos = target_pos
+                mdf.ix[dd, 'cost'] -=  abs(target_pos) * (offset + target*tcost)
         mdf.ix[dd, 'pos'] = pos
     (res_pnl, ts) = backtest.get_pnl_stats( mdf, start_equity, marginrate, 'm')
     res_trade = backtest.get_trade_stats( closed_trades )
@@ -122,9 +137,10 @@ def gen_config_file(filename):
     chan_func = { 'high': {'func': 'dh.PCT_CHANNEL', 'args':{'pct': 90, 'field': 'high'}},
                   'low':  {'func': 'dh.PCT_CHANNEL', 'args':{'pct': 10, 'field': 'low'}}}
     config = {'capital': 10000,
-              'use_chan': False,
+              'chan': 0,
               'trans_cost': 0.0,
               'close_daily': False,
+              'mode': 'close',
               'unit': 1,
               'stoploss': 0.0,
               'pos_args': {},
