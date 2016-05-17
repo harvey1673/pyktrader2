b4b5bd21d5236429c183d3d50b8261b7d9c08537
harveywwu <harvey_wwu@hotmail.com>
2016-05-17 19:22:44 +0800

    Update bktest_dt_stop.py

--- bktest_dt_stop.py
+++ bktest_dt_stop.py
@@ -10,6 +10,8 @@ import json
 import backtest
 
 def dual_thrust_sim( mdf, config):
+    (pos_class, pos_args) = config['pos_param']
+    update_freq = config.get('update_freq', 1)
     ddf = config['ddf']
     close_daily = config['close_daily']
     marginrate = config['marginrate']
@@ -18,15 +20,15 @@ def dual_thrust_sim( mdf, config):
     win = config['param'][1]
     multiplier = config['param'][2]
     f = config['param'][3]
-    ep_enabled = config['EP']
     start_equity = config['capital']
     chan = config['chan']
     chan_func = config['chan_func']
+    use_chan = config('use_chan']
     tcost = config['trans_cost']
     unit = config['unit']
     SL = config['stoploss']
     min_rng = config['min_range']
-    no_trade_set = config['no_trade_set']
+
     if win == -1:
         tr= pd.concat([ddf.high - ddf.low, ddf.close - ddf.close.shift(1)], 
                        join='outer', axis=1).max(axis=1).shift(1)
@@ -41,17 +43,20 @@ def dual_thrust_sim( mdf, config):
                        pd.rolling_max(ddf.close, win) - pd.rolling_min(ddf.low, win)], 
                        join='outer', axis=1).max(axis=1).shift(1)
     ddf['TR'] = tr
-    ddf['MA'] = pd.rolling_mean(ddf.close, chan).shift(1)
-    ddf['H1'] = eval(chan_func['high']['func'])(ddf, chan, **chan_func['high']['args']).shift(1)
-    ddf['L1'] = eval(chan_func['low']['func'])(ddf, chan, **chan_func['low']['args']).shift(1)
+    ddf['CH_H'] = eval(chan_func['high']['func'])(ddf, chan, **chan_func['high']['args']).shift(1)
+    ddf['CH_L'] = eval(chan_func['low']['func'])(ddf, chan, **chan_func['low']['args']).shift(1)
     ll = mdf.shape[0]
     mdf['pos'] = pd.Series([0]*ll, index = mdf.index)
     mdf['cost'] = pd.Series([0]*ll, index = mdf.index)
-    curr_pos = []
-    closed_trades = []
     start_d = ddf.index[0]
     end_d = mdf.index[-1].date()
-    prev_d = start_d - datetime.timedelta(days=1)
+    prev_d = start_d - datetime.timedelta(days=1)    
+    # if close_daily:
+        # mdf['close_ind'] = (mdf.min_id >= config['exit_min'])
+    # else:
+        # mdf['close_ind'] = (mdf.min_id >= config['exit_min']) & (mdf.date == end_d)
+    curr_pos = []
+    closed_trades = []
     tradeid = 0
     for dd in mdf.index:
         mslice = mdf.ix[dd]
@@ -59,6 +64,8 @@ def dual_thrust_sim( mdf, config):
         d = mslice.date
         dslice = ddf.ix[d]
         if np.isnan(dslice.TR) or (mslice.close == 0):
+            continue
+        if use_chan and np.isnan(mslice.CH_H):
             continue
         if len(curr_pos) == 0:
             pos = 0
@@ -80,33 +87,18 @@ def dual_thrust_sim( mdf, config):
         prev_d = d
         buytrig  = d_open + rng
         selltrig = d_open - rng
-        if dslice.MA > mslice.close:
-            buytrig  += f * rng
-        elif dslice.MA < mslice.close:
-            selltrig -= f * rng
-        if ep_enabled:
-            buytrig = max(buytrig, d_high)
-            selltrig = min(selltrig, d_low)
-        if (min_id >= config['exit_min']) :
-            if (pos != 0) and (close_daily or (d == end_d)):
+        if pos != 0:
+            if (mslice.min_id + 1) % update_freq == 0:
+                curr_pos[0].update_bar(mslice)
+            if (curr_pos[0].check_exit(mslice.close, SL)) or (mslice.min_id >= config['exit_min'] and (close_daily or (d == end_d))):
                 curr_pos[0].close(mslice.close - misc.sign(pos) * offset , dd)
                 tradeid += 1
                 curr_pos[0].exit_tradeid = tradeid
                 closed_trades.append(curr_pos[0])
                 curr_pos = []
-                mdf.ix[dd, 'cost'] -=  abs(pos) * (offset + mslice.close*tcost) 
+                mdf.ix[dd, 'cost'] -=  abs(pos) * (offset + mslice.close*tcost)
                 pos = 0
-        elif min_id not in no_trade_set:
-            if (pos!=0) and (SL>0):
-                curr_pos[0].trail_update(mslice.close)
-                if curr_pos[0].check_exit(mslice.close, SL*mslice.close):
-                    curr_pos[0].close(mslice.close-offset*misc.sign(pos), dd)
-                    tradeid += 1
-                    curr_pos[0].exit_tradeid = tradeid
-                    closed_trades.append(curr_pos[0])
-                    curr_pos = []
-                    mdf.ix[dd, 'cost'] -=  abs(pos) * (offset + mslice.close*tcost)    
-                    pos = 0
+        if mslice.min_id < config['exit_min']:
             if (mslice.high >= buytrig) and (pos <=0 ):
                 if len(curr_pos) > 0:
                     curr_pos[0].close(mslice.close+offset, dd)
@@ -115,8 +107,8 @@ def dual_thrust_sim( mdf, config):
                     closed_trades.append(curr_pos[0])
                     curr_pos = []
                     mdf.ix[dd, 'cost'] -=  abs(pos) * (offset + mslice.close*tcost)
-                if mslice.high >= dslice.H1:
-                    new_pos = strat.TradePos([mslice.contract], [1], unit, mslice.close + offset, mslice.close + offset)
+                if mslice.high >= dslice.CH_H:
+                    new_pos = eval(pos_class)([mslice.contract], [1], unit, mslice.close + offset, mslice.close + offset, **pos_args)
                     tradeid += 1
                     new_pos.entry_tradeid = tradeid
                     new_pos.open(mslice.close + offset, dd)
@@ -131,8 +123,8 @@ def dual_thrust_sim( mdf, config):
                     closed_trades.append(curr_pos[0])
                     curr_pos = []
                     mdf.ix[dd, 'cost'] -=  abs(pos) * (offset + mslice.close*tcost)
-                if mslice.low <= dslice.L1:
-                    new_pos = strat.TradePos([mslice.contract], [1], -unit, mslice.close - offset, mslice.close - offset)
+                if mslice.low <= dslice.CH_L:
+                    new_pos = eval(pos_class)([mslice.contract], [1], -unit, mslice.close - offset, mslice.close - offset)
                     tradeid += 1
                     new_pos.entry_tradeid = tradeid
                     new_pos.open(mslice.close - offset, dd)
@@ -149,13 +141,14 @@ def dual_thrust_sim( mdf, config):
 def gen_config_file(filename):
     sim_config = {}
     sim_config['sim_func']  = 'bktest_dt_chanfilter.dual_thrust_sim'
-    sim_config['scen_keys'] = ['chan', 'param']
+    sim_config['scen_keys'] = ['close_daily', 'pos_param']
     sim_config['sim_name']   = 'DTdchan_'
     sim_config['products']   = ['y', 'p', 'l', 'pp', 'cs', 'a', 'rb', 'SR', 'TA', 'MA', 'i', 'j', 'jd', 'jm', 'ag', 'cu', 'm', 'RM', 'ru']
-    sim_config['start_date'] = '20141101'
-    sim_config['end_date']   = '20160219'
+    sim_config['start_date'] = '20150105'
+    sim_config['end_date']   = '20160513'
     sim_config['need_daily'] = True
-    sim_config['pos_class'] = 'strat.TradePos'
+    sim_config['pos_param'] = [('strat.TradePos', {})]
+    sim_config['close_daily'] = [False, True]
     sim_config['proc_func'] = 'dh.day_split'
     sim_config['offset']    = 1
     chan_func = { 'high': {'func': 'dh.PCT_CHANNEL', 'args':{'pct': 90, 'field': 'high'}},
@@ -163,16 +156,13 @@ def gen_config_file(filename):
     config = {'capital': 10000,
               'use_chan': True,
               'trans_cost': 0.0,
-              'close_daily': False,
               'unit': 1,
               'chan': 20,
               'stoploss': 0.0,
               'min_range': 0.0035,
-              'proc_args': {'minlist':[1500]},
+              'proc_args': {'minlist':[]},
               'pos_args': {},
               'param': (0.8, 0, 0.5, 0),
-              'pos_update': False,
-              'EP': False,
               'chan_func': chan_func,
               }
     sim_config['config'] = config
