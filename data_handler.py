da6b01451de814f137f80ceeacae20942051ea70
harveywwu <harvey_wwu@hotmail.com>
2016-07-19 18:20:54 +0800

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -4,6 +4,31 @@ import numpy as np
 import pandas as pd
 import scipy.stats as stats
 
+class DynamicRecArray(object):
+    def __init__(self, dtype):
+        self.dtype = np.dtype(dtype)
+        self.length = 0
+        self.size = 10
+        self._data = np.empty(self.size, dtype=self.dtype)
+
+    def __len__(self):
+        return self.length
+
+    def append(self, rec):
+        if self.length == self.size:
+            self.size = int(1.5*self.size)
+            self._data = np.resize(self._data, self.size)
+        self._data[self.length] = rec
+        self.length += 1
+
+    def extend(self, recs):
+        for rec in recs:
+            self.append(rec)
+
+    @property
+    def data(self):
+        return self._data[:self.length]
+        
 def ohlcsum(df):
     return pd.Series([df.index[0], df['open'][0], df['high'].max(), df['low'].min(), df['close'][-1], df['volume'].sum()],
                   index = ['datetime', 'open','high','low','close','volume'])
