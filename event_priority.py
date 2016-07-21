20b68c23538d0df3d4acd8920721cf4b64e471ba
harveywwu <harvey_wwu@hotmail.com>
2016-07-21 18:28:55 +0800

    Create event_priority.py

---
+++ event_priority.py
@@ -0,0 +1,19 @@
+#-*- coding:utf-8 -*-
+from eventType import *
+Event_Priority_Basic = {}
+
+Event_Priority_Basic[EVENT_TIMER] = 50
+Event_Priority_Basic[EVENT_LOG] = 1000
+Event_Priority_Basic[EVENT_MARKETDATA] = 50
+Event_Priority_Basic[EVENT_TICK] = 50
+Event_Priority_Basic[EVENT_RTNTRADE] = 40
+Event_Priority_Basic[EVENT_TRADE] = 40
+Event_Priority_Basic[EVENT_RTNORDER] = 40
+Event_Priority_Basic[EVENT_ORDER] = 40
+Event_Priority_Basic[EVENT_POSITION] = 60
+Event_Priority_Basic[EVENT_ERRORDERINSERT] = 40
+Event_Priority_Basic[EVENT_ERRORDERCANCEL] = 40
+Event_Priority_Basic[EVENT_ETRADEUPDATE] = 40
+Event_Priority_Basic[EVENT_DB_WRITE] = 5000
+
+Event_Priority_Realtime = {}
