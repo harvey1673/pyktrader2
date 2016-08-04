02418dd2b6d0503123e5b51ecd39a321c07349cd
harveywwu <harvey_wwu@hotmail.com>
2016-08-04 18:51:29 +0800

    Update bktest_psar_chan.py

--- bktest_psar_chan.py
+++ bktest_psar_chan.py
@@ -48,76 +48,64 @@ def psar_test_sim( mdf, config):
     curr_pos = []
     closed_trades = []
     tradeid = 0
-    for idx, dd in enumerate(mdf.index):
-        mslice = xdf.ix[dd]
-        min_id = mslice.min_id
-        min_cnt = (min_id-300)/100 * 60 + min_id % 100 + 1
+    for idx, dd in enumerate(xdf.index):
+        mslice = xdf.loc[dd]
         if len(curr_pos) == 0:
             pos = 0
         else:
             pos = curr_pos[0].pos
-        xdf.ix[dd, 'pos'] = pos
-        if (mslice.MA == 0):
+        xdf.set_value(dd, 'pos', pos)
+        if np.isnan(mslice.ChanH) or np.isnan(mslice.ChanL):
             continue
 		buy_trig  = (mslice.high >= mslice.chanH) and (mslice.psar_dir > 0)
 		sell_trig = (mslice.low <= mslice.chanL) and (mslice.psar_dir < 0)
 		long_close  = (mslice.low <= mslice.chanL) or (mslice.psar_dir < 0)
 		short_close = (mslice.high >= mslice.chanH) or (mslice.psar_dir > 0)
-        if (min_id >= config['exit_min']) and (close_daily or (mslice.datetime.date == end_d)):
+        if mslice.close_ind:
             if (pos != 0):
                 curr_pos[0].close(mslice.close - misc.sign(pos) * offset , dd)
                 tradeid += 1
                 curr_pos[0].exit_tradeid = tradeid
                 closed_trades.append(curr_pos[0])
                 curr_pos = []
-                mdf.ix[dd, 'cost'] -=  abs(pos) * (offset + mslice.close*tcost) 
+                xdf.set_value(dd, 'cost', xdf.at[dd, 'cost'] - abs(pos) * ( mslice.open * tcost))
+                xdf.set_value(dd, 'traded_price', mslice.open - misc.sign(pos) * offset)
                 pos = 0
-        elif min_id not in no_trade_set:
-            if (pos!=0) and pos_update:
-                curr_pos[0].update_price(mslice.close)
-                if (curr_pos[0].check_exit( mslice.close, stoploss * mslice.close )):
-                    curr_pos[0].close(mslice.close-offset*misc.sign(pos), dd)
-                    tradeid += 1
-                    curr_pos[0].exit_tradeid = tradeid
-                    closed_trades.append(curr_pos[0])
-                    curr_pos = []
-                    mdf.ix[dd, 'cost'] -=  abs(pos) * (offset + mslice.close*tcost)    
-                    pos = 0
-			close_price = mslice.close
-            if (short_close or long_close) and (pos != 0):
-				if (mslice.psar_dir > 0) and (pos < 0):
-					close_price = max(mslice.psar, mslice.open)
-				elif (mslice.psar_dir < 0) and (pos < 0):
-					close_price = min(mslice.psar, mslice.open)
-                curr_pos[0].close(mslice.close+offset, dd)
+        else:
+            if (short_close and (pos > 0)) or (long_close and (pos < 0)):
+                curr_pos[0].close(mslice.open - misc.sign(pos) * offset, dd)
                 tradeid += 1
                 curr_pos[0].exit_tradeid = tradeid
                 closed_trades.append(curr_pos[0])
                 curr_pos = []
-                mdf.ix[dd, 'cost'] -=  abs(pos) * (offset + mslice.close*tcost)
-            if buy_trig:
-                new_pos = pos_class([mslice.contract], [1], unit, mslice.close + offset, mslice.close + offset, **pos_args)
+                xdf.set_value(dd, 'cost', xdf.at[dd, 'cost'] - abs(pos) * (mslice.open * tcost))
+                xdf.set_value(dd, 'traded_price', mslice.open - misc.sign(pos) * offset)
+                pos = 0       
+            if (buy_trig or sell_trig) and (pos==0):
+                target_pos = buy_trig * unit - sell_trig * unit
+                new_pos = pos_class([mslice.contract], [1], target_pos, mslice.open, mslice.open, **pos_args)
                 tradeid += 1
                 new_pos.entry_tradeid = tradeid
-                new_pos.open(mslice.close + offset, dd)
+                new_pos.open(mslice.open + misc.sign(target_pos)*offset, dd)
                 curr_pos.append(new_pos)
-                pos = unit
-                mdf.ix[dd, 'cost'] -=  abs(pos) * (offset + mslice.close*tcost)
-            elif sell_trig:
-                new_pos = pos_class([mslice.contract], [1], -unit, mslice.close - offset, mslice.close - offset, **pos_args)
-                tradeid += 1
-                new_pos.entry_tradeid = tradeid
-                new_pos.open(mslice.close - offset, dd)
-                curr_pos.append(new_pos)
-                pos = -unit
-                mdf.ix[dd, 'cost'] -= abs(pos) * (offset + mslice.close*tcost)
-        mdf.ix[dd, 'pos'] = pos
-            
-    (res_pnl, ts) = backtest.get_pnl_stats( mdf, start_equity, marginrate, 'm')
-    res_trade = backtest.get_trade_stats( closed_trades )
-    res = dict( res_pnl.items() + res_trade.items())
-    return (res, closed_trades, ts)
-
+                pos = target_pos
+                xdf.set_value(dd, 'cost', xdf.at[dd, 'cost'] -  abs(target_pos) * (mslice.open * tcost))
+                xdf.set_value(dd, 'traded_price', mslice.open + misc.sign(target_pos)*offset)
+        #if pos_update and pos != 0:
+        #    if curr_pos[0].check_exit(mslice.open, stoploss * mslice.boll_std):
+        #        curr_pos[0].close(mslice.open - misc.sign(pos) * offset, dd)
+        #        tradeid += 1
+        #        curr_pos[0].exit_tradeid = tradeid
+        #        closed_trades.append(curr_pos[0])
+        #        curr_pos = []
+        #        xdf.set_value(dd, 'cost', xdf.at[dd, 'cost'] - abs(pos) * (mslice.open * tcost))
+        #        xdf.set_value(dd, 'traded_price', mslice.open - misc.sign(pos) * offset)
+        #        pos = 0
+        #    else:
+        #        curr_pos[0].update_bar(mslice)                
+        xdf.set_value(dd, 'pos', pos)
+    return (xdf, closed_trades)
+    
 def gen_config_file(filename):
     sim_config = {}
     sim_config['sim_func']  = 'bktest_psar_test.psar_test_sim'
