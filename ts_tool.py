a3771884757de260cf3f57c6fd28ee72c1fc5b50
harveywwu <harvey_wwu@hotmail.com>
2016-10-07 19:00:02 +0800

    Update ts_tool.py

--- ts_tool.py
+++ ts_tool.py
@@ -1,13 +1,57 @@
 import backtest
 import misc
-import pandas as pd
-import numpy as np
 import math
 import mysqlaccess
 import data_handler as dh
 from scipy.stats import norm
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+import pandas.io.data as web
+import pprint
 import statsmodels.tsa.stattools as ts
+from pandas.stats.api import ols
 
+def plot_price_series(df, ts_lab1, ts_lab2):
+    #months = mdates.MonthLocator()  # every month
+    fig, ax = plt.subplots()
+    ax.plot(df.index, df[ts_lab1], label=ts_lab1)
+    ax.plot(df.index, df[ts_lab2], label=ts_lab2)
+    #ax.xaxis.set_major_locator(months)
+    #ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
+    ax.grid(True)
+    fig.autofmt_xdate()
+    plt.xlabel('Month/Year')
+    plt.ylabel('Price ($)')
+    plt.title('%s and %s Daily Prices' % (ts_lab1, ts_lab2))
+    plt.legend()
+    plt.show()
+
+def plot_scatter_series(df, ts_lab1, ts_lab2):
+    plt.xlabel('%s Price ($)' % ts_lab1)
+    plt.ylabel('%s Price ($)' % ts_lab2)
+    plt.title('%s and %s Price Scatterplot' % (ts_lab1, ts_lab2))
+    plt.scatter(df[ts_lab1], df[ts_lab2])
+    plt.show()
+
+def plot_series(ts):
+    #months = mdates.MonthLocator()  # every month
+    fig, ax = plt.subplots()
+    ax.plot(df.index, ts, label=ts.name)
+    #ax.xaxis.set_major_locator(months)
+    #ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
+    #ax.set_xlim(datetime.datetime(2012, 1, 1), datetime.datetime(2013, 1, 1))
+    ax.grid(True)
+    fig.autofmt_xdate()
+    plt.xlabel('Month/Year')
+    plt.ylabel('Price ($)')
+    plt.title('Residual Plot')
+    plt.legend()
+    plt.plot(ts)
+    plt.show()
+    
 def get_cont_data(asset, start_date, end_date, freq = '1m', nearby = 1, rollrule = '-10b'):
     if nearby == 0:
         mdf = mysqlaccess.load_min_data_to_df('fut_min', asset, start_date, end_date, minid_start = 300, minid_end = 2114, database = 'hist_data')
@@ -66,3 +110,23 @@ def hurst(ts, max_shift = 100):
 
 def adf_test(tseries, order = 1):
     return ts.adfuller(tseries, order)
+
+def cadf_test(df1, df2, sdat, edate, idx = 'close', order = 1):
+    df = pd.concat([df1[idx], df2[idx]], axis = 1, keys = ['asset1', 'asset2'])
+    plot_price_series(df, 'asset1', 'asset2')
+    plot_scatter_series(df, 'asset1', 'asset2')
+    res = ols(y = df['asset1'], x = df['asset2'])
+    beta_hr = res.beta.x
+    res = df['asset1'] - beta_hr*df['asset2']
+    plot_series(res)
+    cadf = adf_test(res, order)
+    pprint.pprint(cadf)
+
+    res = ols(y = df['asset2'], x = df['asset1'])
+    beta_hr = res.beta.x
+    res = df['asset2'] - beta_hr*df['asset1']
+    plot_series(res)
+    cadf = adf_test(res, order)
+    pprint.pprint(cadf)
+    
+ 
