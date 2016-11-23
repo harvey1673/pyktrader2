b12d77db57c4d4de8be3f2280d7df5a85957a43e
harveywwu <harvey_wwu@hotmail.com>
2016-11-24 00:48:01 +0800

    nothing to update

--- trade_executor.py
+++ trade_executor.py
@@ -5,7 +5,12 @@ from misc import *
 import datetime
 import order
 
-class TradeExecutor(object):
-    def __init__(self, etrade):
+class TradeExecBase(object):
+    def __init__(self, etrade, use_fak = True):
+        self.use_fak = use_fak
+        self.child_orders = []
+        self.filled_vol = 0
+        pass
 
+    def update_status():
         pass
