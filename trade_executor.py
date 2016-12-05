fca6d3df9f7be2b67a1599a6176f5c5702cc43a4
harveywwu <harvey_wwu@hotmail.com>
2016-12-05 19:00:25 +0800

    Update trade_executor.py

--- trade_executor.py
+++ trade_executor.py
@@ -6,13 +6,22 @@ import datetime
 import order
 
 class TradeExecBase(object):
-    def __init__(self, etrade, use_fak = True):
-        self.use_fak = use_fak
-        self.etrade = etrade
-        self.child_orders = []
-        self.filled_vol = 0
+    def __init__(self, etrade):
+        self.etrade = etrade        
 
     def update_status(self, status):
         self.etrade.status = status
 
-    def
+    def unwind(self):
+        pass
+    
+    def update(self):
+        self.etrade.update()
+    
+    def resume(self):
+        pass
+    
+    def suspend(self):
+        pass       
+    
+        
