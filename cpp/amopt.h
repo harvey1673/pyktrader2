fd22496ab03c55ca6995def43ca90d8e787d1b62
harveywwu <harvey_wwu@hotmail.com>
2017-03-09 11:57:02 +0800

    Update amopt.h

--- cpp/amopt.h
+++ cpp/amopt.h
@@ -8,9 +8,9 @@
 #include "euopt.h"
 #include "math_utils.h"
 
-double american_prem_fut_btree(double F,double K,double vol,double t_exp,double df, std::string PutCall);
-double AmericanOptFutPrice(double F,double K,double vol,double t_exp,double df, std::string PutCall);
-double AmericanImpliedVol(double MktPrice, double F,double K,double r,double T, std::string PutCall);
+double american_prem_fut_btree(double F,double K,double vol,double t_exp,double df, std::string PutCall, int bin_tree_nstep = 128);
+double AmericanOptFutPrice(double F,double K,double vol,double t_exp,double df, std::string PutCall, int bin_tree_nstep = 128);
+double AmericanImpliedVol(double MktPrice, double F,double K,double r,double T, std::string PutCall, int bin_tree_nstep = 128);
 //double american_prem_fut_BAW(double F,double K,double vol,double t_exp,double df, std::string PutCall);
 
 #endif
