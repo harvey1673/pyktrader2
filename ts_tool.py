bcd15cf06e3613ff7c25d99c76ac8cbfe57bb271
harveywwu <harvey_wwu@hotmail.com>
2017-07-28 00:19:18 +0800

    update

--- ts_tool.py
+++ ts_tool.py
@@ -81,7 +81,6 @@ def validate_db_data(tday, filter = False):
             inst_list['min'].append(output)        
     print inst_list
 
-
 def variance_ratio(ts, freqs):
     data = ts.values
     nlen = len(data)
@@ -167,21 +166,20 @@ def half_time(ts):
     return -np.log(2)/res[0]
     
 def get_johansen(y, p):
-        """
-        Get the cointegration vectors at 95% level of significance
-        given by the trace statistic test.
-        """
-        N, l = y.shape
-        jres = coint_johansen(y, 0, p)
-        trstat = jres.lr1                       # trace statistic
-        tsignf = jres.cvt                       # critical values
-
-        for i in range(l):
-            if trstat[i] > tsignf[i, 1]:     # 0: 90%  1:95% 2: 99%
-                r = i + 1
-        jres.r = r
-        jres.evecr = jres.evec[:, :r]
-        return jres
+    """
+    Get the cointegration vectors at 95% level of significance
+    given by the trace statistic test.
+    """
+    N, l = y.shape
+    jres = coint_johansen(y, 0, p)
+    trstat = jres.lr1                       # trace statistic
+    tsignf = jres.cvt                       # critical values
+    for i in range(l):
+        if trstat[i] > tsignf[i, 1]:     # 0: 90%  1:95% 2: 99%
+            r = i + 1
+    jres.r = r
+    jres.evecr = jres.evec[:, :r]
+    return jres
         
 def signal_stats(df, signal, time_limit = None):
     long_signal = pd.Series(np.nan, index = df.index)
