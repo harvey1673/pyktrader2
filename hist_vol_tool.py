c304f47dc5d734b8732f1172cf0f2196e8a90ae4
harveywwu <harvey_wwu@hotmail.com>
2017-09-09 19:09:21 +0800

    Update hist_vol_tool.py

--- hist_vol_tool.py
+++ hist_vol_tool.py
@@ -5,7 +5,8 @@ import pyktlib
 import pandas as pd
 import numpy as np
 import math
-import mysqlaccess
+import mysql.connector as sqlconn
+import dbaccess
 import backtest
 import data_handler as dh
 from scipy.stats import norm
@@ -84,9 +85,12 @@ def realized_vol(df, option_input, calib_input, column = 'close'):
     else :
         return vol
 
-def bs_delta_to_ratio(delta, vol, texp):
+def bs_delta_to_strike(fwd, delta, vol, texp):
     ys = norm.ppf(delta)
-    return math.exp((ys - 0.5 * vol * math.sqrt(texp)) * vol * math.sqrt(texp))
+    return fwd/math.exp((ys - 0.5 * vol * math.sqrt(texp)) * vol * math.sqrt(texp))
+
+def bachelier_delta_to_strike(fwd, delta, vol, texp):
+    return fwd - norm.ppf(delta) * vol * math.sqrt(texp)
 
 def realized_termstruct(option_input, data):
     is_dtime = data.get('is_dtime', False)
@@ -94,7 +98,7 @@ def realized_termstruct(option_input, data):
     xs = data.get('xs', [0.5])
     xs_cols = data.get('xs_names', ['atm'])
     use_bootstrap = data.get('use_bootstrap', False)
-    xs_func = data.get('xs_func', 'bs_delta_to_ratio')
+    xs_func = data.get('xs_func', 'bs_delta_to_strike')
     xs_func = eval(xs_func)
     term_tenor = data.get('term_tenor', '-1m')
     df = data['dataframe']
@@ -138,7 +142,7 @@ def realized_termstruct(option_input, data):
             strike = sub_df[column].iat[0]
             texp = (expiry.date() - start_d).days/YEARLY_DAYS
             if idx > 0:
-                strike = strike / xs_func(xs[idx], vols[0], texp)
+                strike = xs_func(strike, xs[idx], vols[0], texp)
             option_input['strike'] = strike
             if end_vol > 0:
                 tau = max((expiry.date() - end_d).days, 1.0)/YEARLY_DAYS
@@ -160,7 +164,7 @@ def realized_termstruct(option_input, data):
     return vol_ts
 
 def hist_realized_vol_by_product(prodcode, start_d, end_d, periods = 12, tenor = '-1m', writeDB = False):
-    cont_mth, exch = mysqlaccess.prod_main_cont_exch(prodcode)
+    cont_mth, exch = dbaccess.prod_main_cont_exch(prodcode)
     contlist = contract_range(prodcode, exch, cont_mth, start_d, end_d)
     exp_dates = [get_opt_expiry(cont, inst2contmth(cont)) for cont in contlist]
     data = {'is_dtime': True,
@@ -168,7 +172,7 @@ def hist_realized_vol_by_product(prodcode, start_d, end_d, periods = 12, tenor =
             'data_freq': '30min',
             'xs': [0.5, 0.25, 0.75],
             'xs_names': ['atm', 'v25', 'v75'],
-            'xs_func': 'bs_delta_to_ratio',
+            'xs_func': 'bs_delta_to_strike',
             'rehedge_period': 1,
             'term_tenor': tenor,
             'database': 'hist_data'
@@ -189,14 +193,71 @@ def hist_realized_vol_by_product(prodcode, start_d, end_d, periods = 12, tenor =
             break
         p_str = '-' + str(int(tenor[1:-1]) * periods) + tenor[-1]
         d_start = day_shift(expiry_d, p_str)
+        cnx = sqlconn.connect(**dbaccess.dbconfig)
         if freq == 'd':
-            df = mysqlaccess.load_daily_data_to_df('fut_daily', cont, d_start, expiry_d, database=data['database'], index_col = None)
+            df = dbaccess.load_daily_data_to_df(cnx, 'fut_daily', cont, d_start, expiry_d, index_col = None)
         else:
-            mdf = mysqlaccess.load_min_data_to_df('fut_min', cont, d_start, expiry_d, minid_start=300,
-                                                  minid_end=2115, database = data['database'], index_col = None)
+            mdf = dbaccess.load_min_data_to_df(cnx, 'fut_min', cont, d_start, expiry_d, minid_start=300,
+                                                  minid_end=2115, index_col = None)
             mdf = backtest.cleanup_mindata(mdf, prodcode, index_col = None)
             mdf['bar_id'] = dh.bar_conv_func2(mdf['min_id'])
             df = dh.conv_ohlc_freq(mdf, freq, bar_func=dh.bar_conv_func2, extra_cols=['bar_id'], index_col = None)
+        cnx.close()
+        option_input['expiry'] = expiry
+        data['dataframe'] = df
+        vol_df = realized_termstruct(option_input, data)
+        print cont, expiry_d, vol_df
+
+def hist_cso_by_product(prodcode, start_d, end_d, periods = 24, tenor = '-1w', max_spd = 2, writeDB = False, mode = 'n'):
+    cont_mth, exch = dbaccess.prod_main_cont_exch(prodcode)
+    contlist = contract_range(prodcode, exch, cont_mth, start_d, end_d)
+    exp_dates = [get_opt_expiry(cont, inst2contmth(cont)) for cont in contlist]
+    if mode == 'n':
+        xs_func = 'bachelier_delta_to_strike'
+        pricer_func = 'bsopt.BSFwdNormal'
+        delta_func = 'bsopt.BSFwdNormalDelta'
+    else:
+        xs_func = 'bs_delta_to_strike'
+        pricer_func = 'bsopt.BSOpt'
+        delta_func = 'bsopt.BSDelta'
+    data = {'is_dtime': True,
+            'data_column': 'close',
+            'data_freq': '30min',
+            'xs': [0.5, 0.25, 0.75],
+            'xs_names': ['atm', 'v25', 'v75'],
+            'xs_func': xs_func,
+            'rehedge_period': 1,
+            'term_tenor': tenor,
+            'database': 'hist_data'
+            }
+    option_input = {'otype': True,
+                    'rd': 0.0,
+                    'rf': 0.0,
+                    'end_vol': 0.0,
+                    'ref_vol': 0.5,
+                    'pricer_func': pricer_func,
+                    'delta_func': delta_func,
+                    'is_dtime': data['is_dtime'],
+                    }
+    freq = data['data_freq']
+    dbconfig = copy.deepcopy(**dbaccess.hist_dbconfig)
+    dbconfig['database'] = data['database']
+    cnx = sqlconn.connect(**dbconfig)
+    for cont, expiry in zip(contlist, exp_dates):
+        expiry_d = expiry.date()
+        if expiry_d > end_d:
+            break
+        p_str = '-' + str(int(tenor[1:-1]) * periods) + tenor[-1]
+        d_start = day_shift(expiry_d, p_str)
+        if freq == 'd':
+            df = dbaccess.load_daily_data_to_df(cnx, 'fut_daily', cont, d_start, expiry_d, index_col = None)
+        else:
+            mdf = dbaccess.load_min_data_to_df(cnx, 'fut_min', cont, d_start, expiry_d, minid_start=300,
+                                                  minid_end=2115, index_col = None)
+            mdf = backtest.cleanup_mindata(mdf, prodcode, index_col = None)
+            mdf['bar_id'] = dh.bar_conv_func2(mdf['min_id'])
+            df = dh.conv_ohlc_freq(mdf, freq, bar_func=dh.bar_conv_func2, extra_cols=['bar_id'], index_col = None)
+        cnx.close()
         option_input['expiry'] = expiry
         data['dataframe'] = df
         vol_df = realized_termstruct(option_input, data)
@@ -208,7 +269,8 @@ def volgrid_hist_slice(underlier, strike_list, curr_date, tick_id, accr = 'COMN1
         iv_func = pyktlib.BlackImpliedVol
     else:
         iv_func = pyktlib.AmericanImpliedVol
-    df = mysqlaccess.load_tick_to_df('fut_tick', underlier, curr_date, curr_date, start_tick=300000, end_tick = tick_id)
+    cnx = sqlconn.connect(**dbaccess.dbconfig)
+    df = dbaccess.load_tick_to_df(cnx, 'fut_tick', underlier, curr_date, curr_date, start_tick=300000, end_tick = tick_id)
     slice_tick = df['tick_id'].iat[-1]
     under_bid = df['bidPrice1'].iat[-1]
     under_ask = df['askPrice1'].iat[-1]
@@ -222,7 +284,7 @@ def volgrid_hist_slice(underlier, strike_list, curr_date, tick_id, accr = 'COMN1
         res[strike] = {}
         for otype in ['C', 'P']:
             opt_inst = get_opt_name(underlier, otype, strike)
-            xdf = mysqlaccess.load_tick_to_df('fut_tick', opt_inst, curr_date, curr_date, start_tick=300000,
+            xdf = dbaccess.load_tick_to_df(cnx, 'fut_tick', opt_inst, curr_date, curr_date, start_tick=300000,
                                              end_tick=slice_tick)
             if len(xdf) == 0:
                 print opt_inst
@@ -235,6 +297,7 @@ def volgrid_hist_slice(underlier, strike_list, curr_date, tick_id, accr = 'COMN1
                     ivol = iv_func(*iv_args)
                     key = otype + '-' + tag
                     res[strike][key] = ivol
+    cnx.close()
     return pd.DataFrame.from_dict(res, orient = 'index')[['C-bvol','C-avol','P-bvol','P-avol']]
 
 def variance_ratio(ts, freqs):
@@ -262,17 +325,55 @@ def validate_db_data(tday, filter = False):
     all_insts = filter_main_cont(tday, filter)
     data_count = {}
     inst_list = {'min': [], 'daily': [] }
+    cnx = sqlconn.connect(**dbaccess.dbconfig)
     for instID in all_insts:
-        df = mysqlaccess.load_daily_data_to_df('fut_daily', instID, tday, tday)
+        df = dbaccess.load_daily_data_to_df(cnx, 'fut_daily', instID, tday, tday)
         if len(df) <= 0:
             inst_list['daily'].append(instID)
         elif (df.close[-1] == 0) or (df.high[-1] == 0) or (df.low[-1] == 0) or df.open[-1] == 0:
             inst_list['daily'].append(instID)
-        df = mysqlaccess.load_min_data_to_df('fut_min', instID, tday, tday, minid_start=300, minid_end=2115, database='blueshale')
+        df = dbaccess.load_min_data_to_df(cnx, 'fut_min', instID, tday, tday, minid_start=300, minid_end=2115, database='blueshale')
         if len(df) <= 100:
             output = instID + ':' + str(len(df))
             inst_list['min'].append(output)
         elif df.min_id < 2055:
             output = instID + ': end earlier'
-            inst_list['min'].append(output)        
+            inst_list['min'].append(output)
+    cnx.close()
     print inst_list
+
+def hist_vol_by_spot(spotID, start_d, end_d, periods = 1, tenor = '-1m', writeDB = False):
+    data = {'is_dtime': False,
+            'data_column': 'price',
+            'data_freq': 'd',
+            'xs': [0.5],
+            'xs_names': ['atm'],
+            'xs_func': 'bs_delta_to_strike',
+            'rehedge_period': 3,
+            'term_tenor': tenor,
+            }
+    option_input = {'otype': True,
+                    'rd': 0.0,
+                    'rf': 0.0,
+                    'end_vol': 0.0,
+                    'ref_vol': 0.5,
+                    'pricer_func': 'bsopt.BSOpt',
+                    'delta_func': 'bsopt.BSDelta',
+                    'is_dtime': data['is_dtime'],
+                    }
+    cnx = sqlconn.connect(**dbaccess.dbconfig)
+    p_str = '-' + str(int(tenor[1:-1]) * periods) + tenor[-1]
+    d_end = end_d
+    res = {}
+    while d_end > start_d:
+        d_start = day_shift(d_end, p_str)
+        xdf = dbaccess.load_daily_data_to_df(cnx, 'spot_daily', spotID, d_start, d_end, index_col=None, field = 'spotID')
+        xdf['date'] = pd.to_datetime(xdf['date'])
+        option_input['expiry'] = datetime.datetime.combine(d_end, datetime.time(0, 0))
+        data['dataframe'] = xdf
+        vol_df = realized_termstruct(option_input, data)
+        res[d_start] = vol_df['atm'].values
+        d_end = day_shift(d_end, tenor)
+    cnx.close()
+    df = pd.DataFrame.from_dict(res, orient = 'index').sort_index()
+    return df
