a9c26e2e078bd837d72b1c743bfa1d77f439eb41
harveywwu <harvey_wwu@hotmail.com>
2017-09-09 19:01:50 +0800

    Update ts_tool.py

--- ts_tool.py
+++ ts_tool.py
@@ -3,7 +3,8 @@ import backtest
 import misc
 import math
 import bsopt
-import mysqlaccess
+import dbaccess
+import sqlite3 as sqlconn
 import data_handler as dh
 import datetime
 import numpy as np
@@ -53,8 +54,9 @@ def plot_series(ts):
     plt.show()
     
 def get_cont_data(asset, start_date, end_date, freq = '1m', nearby = 1, rollrule = '-10b'):
+    cnx = sqlconn.connect(**dbaccess.hist_dbconfig)
     if nearby == 0:
-        mdf = mysqlaccess.load_min_data_to_df('fut_min', asset, start_date, end_date, minid_start = 300, minid_end = 2114, database = 'hist_data')
+        mdf = dbaccess.load_min_data_to_df(cnx, 'fut_min', asset, start_date, end_date, minid_start = 300, minid_end = 2114, database = 'hist_data')
         mdf['contract'] = asset
     else:
         mdf = misc.nearby(asset, nearby, start_date, end_date, rollrule, 'm', need_shift=True, database = 'hist_data')
@@ -66,13 +68,14 @@ def validate_db_data(tday, filter = False):
     all_insts = misc.filter_main_cont(tday, filter)
     data_count = {}
     inst_list = {'min': [], 'daily': [] }
+    cnx = sqlconn.connect(**dbaccess.dbconfig)
     for instID in all_insts:
-        df = mysqlaccess.load_daily_data_to_df('fut_daily', instID, tday, tday)
+        df = dbaccess.load_daily_data_to_df(cnx, 'fut_daily', instID, tday, tday)
         if len(df) <= 0:
             inst_list['daily'].append(instID)
         elif (df.close[-1] == 0) or (df.high[-1] == 0) or (df.low[-1] == 0) or df.open[-1] == 0:
             inst_list['daily'].append(instID)
-        df = mysqlaccess.load_min_data_to_df('fut_min', instID, tday, tday, minid_start=300, minid_end=2115, database='blueshale')
+        df = dbaccess.load_min_data_to_df(cnx, 'fut_min', instID, tday, tday, minid_start=300, minid_end=2115)
         if len(df) <= 100:
             output = instID + ':' + str(len(df))
             inst_list['min'].append(output)
