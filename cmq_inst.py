189f0f857d1ca8c59012cfd815179cf8abe5630f
harveywwu <harvey_wwu@hotmail.com>
2017-09-20 18:07:51 +0800

    Update cmq_inst.py

--- cmq_inst.py
+++ cmq_inst.py
@@ -4,22 +4,26 @@ import cmq_utils
 
 class CMQInstrument(object):
     def __init__(self, trade_data, market_data, model_settings={}):
-        self.price_func_key = 'clean_price'
         self.set_trade_data(trade_data)
         if len(market_data) > 0:
             self.set_market_data(market_data)
         if len(model_settings) > 0:
             self.set_model_settings(model_settings)
+        self.unique_id = self.generate_unique_id()
+
+    def generate_unique_id(self):
+        return  '_'.join([str(key) for key in self.inst_key])
 
     def set_market_data(self, market_data):
         self.value_date = market_data['MarketDate']
 
     def set_trade_data(self, trade_data):
-        self.pricing_ccy = trade_data.get('PricingCCY', 'USD')
+        self.pricing_ccy = trade_data.get('PricingCcy', 'USD')
         self.notional = trade_data.get('Notional', 1.0)
+        self.inst_key = [self.__class__.__name__, self.notional, self.pricing_ccy]
 
     def set_model_settings(self, model_settings):
-        pass
+        self.price_func_key = model_settings.get('PriceFunc', 'clean_price')
 
     def mkt_deps(self):
         return {}
