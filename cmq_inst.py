7f414087a6139f4838ce32cec2775caa519ee779
harveywwu <harvey_wwu@hotmail.com>
2017-09-25 18:34:37 +0800

    Update cmq_inst.py

--- cmq_inst.py
+++ cmq_inst.py
@@ -1,41 +1,51 @@
 # -*- coding:utf-8 -*-
-import copy
-import cmq_utils
+import json
+import datetime
 
 class CMQInstrument(object):
-    def __init__(self, trade_data, market_data, model_settings={}):
-        self.set_trade_data(trade_data)
+    class_params = {'pricing_ccy': 'USD'}
+    def __init__(self, inst_data, market_data, model_settings={}):
+        if isinstance(inst_data, (str, unicode)):
+            inst_data = json.loads(inst_data)
+        self.set_trade_data(inst_data)
         if len(market_data) > 0:
             self.set_market_data(market_data)
         if len(model_settings) > 0:
             self.set_model_settings(model_settings)
-        self.unique_id = self.generate_unique_id()
-
-    def generate_unique_id(self):
-        return  '_'.join([str(key) for key in self.inst_key])
-
-    def set_market_data(self, market_data):
-        self.value_date = market_data['MarketDate']
 
     def set_trade_data(self, trade_data):
-        self.pricing_ccy = trade_data.get('PricingCcy', 'USD')
-        self.notional = trade_data.get('Notional', 1.0)
-        self.inst_key = [self.__class__.__name__, self.notional, self.pricing_ccy]
+        d = self.__dict__
+        for key in self.class_params:
+            d[key] = trade_data.get(key, self.class_params[key])
+        self.set_inst_key()
+        self.mkt_deps = {}
+
+    def set_inst_key(self):
+        self.inst_key = [self.__class__.__name__, self.pricing_ccy]
+        self.generate_unique_id()
+
+    def generate_unique_id(self):
+        self.unique_id = '_'.join([str(key) for key in self.inst_key])
 
     def set_model_settings(self, model_settings):
-        self.price_func_key = model_settings.get('PriceFunc', 'clean_price')
+        self.price_func = model_settings.get('price_func', 'clean_price')
 
-    def mkt_deps(self):
-        return {}
+    def set_market_data(self, market_data):
+        self.value_date = market_data.get('value_date', datetime.date.today())
 
     def price(self):
-        return getattr(self, self.price_func_key)
+        return getattr(self, self.price_func)
 
     def clean_price(self):
         return 0.0
 
     def dirty_price(self):
-        return 0.0
+        return self.clean_price()
 
     def inst_key(self):
         return self.__class__.__name__
+
+    def __str__(self):
+        output = dict([(param, getattr(self, param)) for param in self.class_params])
+        return json.dumps(output)
+
