9e1220faa2fb4ea3ed64c4b1699056b052b90ae4
harveywwu <harvey_wwu@hotmail.com>
2017-11-07 18:49:06 +0800

    Update cmq_normcso.py

--- cmq_normcso.py
+++ cmq_normcso.py
@@ -15,8 +15,9 @@ class CMQNormalCSO(CMQInstrument):
                                                         'leg_b': datetime.date(2018, 2, 1),
                                                         'accrual': 'act252',
                                                         'otype': 'C',
+                                                        'leg_diff': 1,
                                                         'need_disc': True})
-    inst_key = ['fwd_index', 'contract', 'otype', 'strike', 'end', 'ccy']
+    inst_key = ['fwd_index', 'leg_a', 'leg_b', 'otype', 'strike', 'end', 'ccy']
 
     def __init__(self, trade_data, market_data = {}, model_settings = {}):
         super(CMQNormalCSO, self).__init__(trade_data, market_data, model_settings)
@@ -25,7 +26,7 @@ class CMQNormalCSO(CMQInstrument):
     def set_trade_data(self, trade_data):
         super(CMQNormalCSO, self).set_trade_data(trade_data)
         self.mkt_deps['COMFwd'] = { self.fwd_index: [self.leg_a, self.leg_b] }
-        self.leg_diff = min(abs((self.leg_b.year * 12 + self.leg_b.month) - (self.leg_a.year * 12 + self.leg_b.month)), 4)
+        self.leg_diff = 1
         self.mkt_deps['COMDV'+str(self.leg_diff)] = { self.fwd_index: [self.leg_a] }
         if self.need_disc:
             self.mkt_deps['IRCurve'] = { self.ccy.lower() + '_disc': ['ALL'] }
@@ -45,9 +46,11 @@ class CMQNormalCSO(CMQInstrument):
                 self.fwd_a = quote[2]
                 continue
             elif quote[0] == self.leg_b:
+                self.fwd_b = quote[2]
                 break
-        self.ivol = cmq_crv_defn.lookup_vol_mark(self.fwd_index, market_data, self.end, \
-                                                    vol_fields=cmq_crv_defn.COMVOL_fields)
+        volmark = cmq_crv_defn.lookup_vol_mark(self.fwd_index, market_data, self.end, \
+                                                    vol_fields=['COMDV'+str(self.leg_diff)])
+        self.ivol = volmark['COMDV'+str(self.leg_diff)]
         if self.need_disc and (self.end >= self.value_date):
             rate_quotes = market_data['IRCurve'][self.ccy.lower() + '_disc']
             tenors = [(quote[1] - self.value_date).days for quote in rate_quotes]
@@ -61,18 +64,8 @@ class CMQNormalCSO(CMQInstrument):
 
     def clean_price(self):
         ws = 1.0 if (self.otype[0].upper() == 'C') else -1.0
-        self.volnode = cmq_volgrid.SamuelDelta5VolNode(self.value_date, \
-                                            self.volmark['expiry'], \
-                                            self.fwd_avg, \
-                                            self.volmark['COMVolATM'], \
-                                            self.volmark['COMVolV90'], \
-                                            self.volmark['COMVolV75'], \
-                                            self.volmark['COMVolV25'], \
-                                            self.volmark['COMVolV10'], \
-                                            self.alpha,\
-                                            self.beta, \
-                                            self.accrual)
-        self.ivol = self.volnode.GetVolByStrike(self.strike, self.end)
-        t_exp = misc.conv_expiry_date(self.value_date, self.end, self.accrual, [])
-        pr = bsopt.BSFwd((ws > 0), self.fwd_avg, self.strike, self.ivol, t_exp, 0) * self.df
+        cal_str = cmq_crv_defn.COM_Curve_Map[self.fwd_index]['calendar'] + '_Holidays'
+        hols = getattr(misc, cal_str)
+        t_exp = misc.conv_expiry_date(self.value_date, self.end, self.accrual, hols)
+        pr = bsopt.BSFwdNormal((ws > 0), self.fwd_a - self.fwd_b, self.strike, self.ivol, t_exp, 0) * self.df
         return pr
