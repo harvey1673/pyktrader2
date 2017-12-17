ac24cef56b941c8076d1ee4fd2546ad86952a9d5
harveywwu <harvey_wwu@hotmail.com>
2017-12-18 01:52:20 +0800

    update

--- backtest.py
+++ backtest.py
@@ -21,7 +21,7 @@ sim_margin_dict = { 'au': 0.06, 'ag': 0.08, 'cu': 0.07, 'al':0.05,
                 'IH': 0.01, 'FG': 0.05, 'TF':0.015, 'OI': 0.05,
                 'T': 0.015, 'MA': 0.06, 'cs': 0.05, 'bu': 0.07, 
                 'sn': 0.05, 'v': 0.05, 'hc': 0.09, 'SM': 0.1,
-                'CY': 0.05,}
+                'SF': 0.1, 'CY': 0.05,}
 sim_start_dict = { 'c': datetime.date(2008,10,1), 'm': datetime.date(2010,10,1),
     'y': datetime.date(2010,10,1), 'l': datetime.date(2008,10,1), 'rb':datetime.date(2010,10,1),
     'p': datetime.date(2010,10,1), 'cu':datetime.date(2010,10,1), 'al':datetime.date(2010,10,1),
@@ -36,7 +36,7 @@ sim_start_dict = { 'c': datetime.date(2008,10,1), 'm': datetime.date(2010,10,1),
     'IH':datetime.date(2015,5,1),  'IC':datetime.date(2015,5,1),  'cs':datetime.date(2015,2,1),
     'jd':datetime.date(2014,2,1),  'ni':datetime.date(2015,6,1),  'sn':datetime.date(2015,6,1),
     'ZC':datetime.date(2015,12,1), 'hc':datetime.date(2012, 1, 1), 'SM': datetime.date(2016,10,10),
-    'CY': datetime.date(2017, 8, 18),}
+    'SF': datetime.date(2016,10,10), 'CY': datetime.date(2017, 8, 18),}
 
 trade_offset_dict = {
                 'au': 0.05, 'ag': 1,    'cu': 10,   'al':5,
