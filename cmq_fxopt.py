8b9b92dcaea1d4e7abe20a6936285a03c316f64b
harveywwu <harvey_wwu@hotmail.com>
2018-01-26 11:35:36 +0800

    Update cmq_fxopt.py

--- cmq_fxopt.py
+++ cmq_fxopt.py
@@ -1,65 +1,58 @@
-import cmqlib as qlib
-import datetime
-from cmq_inst import CMQInstrument  
-import cmq_utils
+from cmq_fxfwd import CMQFXForward
+import cmq_crv_defn
+import cmq_volgrid
 import cmq_curve
 import misc
+import bsopt
 
-class CMQFXOption(CMQInstrument):
-    class_params = dict(CMQInstrument.class_params, **{ 'ccypair':'EURUSD', 'otype': 'C', \
-                                                        'expiry': datetime.date.today(), 'strike': 0.0})
+class CMQFXOption(CMQFXForward):
+    class_params = dict(CMQFXForward.class_params, **{ 'otype': 'C' })
+    inst_key = ['ccypair', 'otype', 'strike', 'end', 'ccy', 'volume']
+
     def __init__(self, trade_data, market_data = {}, model_settings = {}):
         super(CMQFXOption, self).__init__(trade_data, market_data, model_settings)
-    
-    def set_market_data(self, market_data):
-        self.today = misc.datetime2xl(datetime.datetime.strptime(market_data['MarketDate'], '%Y-%m-%d'))
-        cmq_utils.Date.set_origin(*[int(s) for s in market_data['MarketDate'].split('-')][::-1])
-        super(CMQFXOption, self).set_market_data(market_data)
-        fwd_quotes = market_data["FXFwd_" + self.ccypair]
-        fwdtenors = [misc.datetime2xl(datetime.datetime.strptime(knot[2], '%Y-%m-%d')) for knot in fwd_quotes]
-        spot_date = cmq_utils.Date(str(market_data['MarketDate']))
-        fwds = [knot[1] for knot in fwd_quotes]
-        self.fx_spot = fwds[0]
-        fwdcurve = cmq_curve.ForwardCurve.from_array(fwdtenors, fwds)
-        self.fx_fwd = fwdcurve(self.expiry)
-        vol_field = "FXVOL_" + self.ccypair
-        calendar = cmq_utils.Calendar.US
-        voltenors = [ float(calendar.advance(spot_date, cmq_utils.Period(str(knot[0]))) - spot_date) for knot in market_data[vol_field]['ATM']]
-        volmark = {}
-        volmark['tenor'] = voltenors
-        for field in ['ATM', 'RRd25', 'RRd10', 'BFd25', 'BFd10']:
-            volmark[field] = [market_data[vol_field][field][i][1] for i in range(len(voltenors))]
-        tom = self.expiry - self.today
-        volpts = {}
-        for field in ['ATM', 'RRd25', 'RRd10', 'BFd25', 'BFd10']:
-            if field == 'ATM':
-                mode = cmq_curve.VolCurve.InterpMode.LinearTime
-            else:
-                mode = cmq_curve.VolCurve.InterpMode.SqrtTime
-            vcurve = cmq_curve.VolCurve.from_array( volmark['tenor'], volmark[field], interp_mode = mode )
-            volpts[field] = vcurve(tom)
-        self.atm = volpts['ATM']
-        self.v10 = volpts['BFd10'] + volpts['RRd10']/2.0    
-        self.v90 = volpts['BFd10'] - volpts['RRd10']/2.0 
-        self.v25 = volpts['BFd25'] + volpts['RRd25']/2.0
-        self.v75 = volpts['BFd25'] - volpts['RRd25']/2.0
-        self.volnode = qlib.Delta5VolNode(self.today, self.expiry, self.fx_fwd, self.atm, self.v90, self.v75, self.v25, self.v10, "act365")
-    
+
     def set_trade_data(self, trade_data):
         super(CMQFXOption, self).set_trade_data(trade_data)
-        self.expiry = misc.datetime2xl(datetime.datetime.strptime(trade_data['expiry'], '%Y-%m-%d'))
-        self.set_inst_key()
+        for field in cmq_crv_defn.FXVOL_fields:
+            if field not in self.mkt_deps:
+                self.mkt_deps[field] = {}
+            self.mkt_deps[field][self.ccypair] = { self.ccypair: [ 'ALL'] }
 
-    def set_inst_key(self):
-        self.inst_key = [self.__class__.__name__, self.ccypair, self.otype, self.strike, self.expiry, self.pricing_ccy, self.notional]
-        
-    def price(self):
-        vol = self.volnode.GetVolByStrike(self.strike)
-        fp = qlib.BlackPrice(self.fx_fwd, self.strike, vol, (self.expiry - self.today)/365.0, 1.0, self.otype)
-        ccy1 = self.ccypair[:3]
-        ccy2 = self.ccypair[-3:]        
-        if self.pricing_ccy == ccy2:
-            mtm = fp * self.notional
+    def set_market_data(self, market_data):
+        super(CMQFXOption, self).set_market_data(market_data)
+        if len(market_data) == 0:
+            self.volmark = None
         else:
-            mtm = fp * self.notional/self.fx_spot
-        return mtm
+            vol_fields = cmq_crv_defn.FXVOL_fields
+            vol_quotes = dict([(field, []) for field in ['expiry'] + vol_fields])
+            for idx, v_quote in enumerate(market_data[vol_fields[0]][self.ccypair]):
+                vol_quotes['expiry'].append(v_quote[1])
+                for field in vol_fields:
+                    vol_quotes[field].append(market_data[field][self.ccypair][idx][2])
+            vol_tenors = [(self.value_date - exp_date).days for exp_date in vol_quotes['expiry']]
+            self.volmark = dict([(field, 0.0) for field in vol_fields])
+            tom = (self.value_date - self.end).days
+            for idx, field in enumerate(vol_fields):
+                if idx == 0:
+                    mode = cmq_curve.VolCurve.InterpMode.LinearTime
+                else:
+                    mode = cmq_curve.VolCurve.InterpMode.SqrtTime
+                vcurve = cmq_curve.VolCurve.from_array( vol_tenors, vol_quotes[field], interp_mode = mode )
+                self.volmark[field] = vcurve(tom)
+
+    def clean_price(self):
+        ws = 1.0 if (self.otype[0].upper() == 'C') else -1.0
+        self.volnode = cmq_volgrid.Delta5VolNode(self.value_date, \
+                                            self.end, \
+                                            self.fx_fwd, \
+                                            self.volmark['FXVolATM'], \
+                                            self.volmark['FXVolV90'], \
+                                            self.volmark['FXVolV75'], \
+                                            self.volmark['FXVolV25'], \
+                                            self.volmark['FXVolV10'], \
+                                            self.accrual)
+        self.ivol = self.volnode.GetVolByStrike(self.strike, self.end)
+        t_exp = misc.conv_expiry_date(self.value_date, self.end, self.accrual, [])
+        pr = bsopt.BSFwd((ws > 0), self.fx_fwd, self.strike, self.ivol, t_exp, 0)/self.fx_fwd * self.df
+        return pr
