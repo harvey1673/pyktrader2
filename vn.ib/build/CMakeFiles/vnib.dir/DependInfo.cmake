9423c3ffd1c0a2441aaa68318c6e1ace9b977f90
harveywwu <harvey_wwu@hotmail.com>
2018-05-03 00:19:49 +0800

    update

---
+++ vn.ib/build/CMakeFiles/vnib.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/harvey/dev/vn.ib.new/vnib/vnib/vnib.cpp" "/home/harvey/dev/vn.ib.new/build/CMakeFiles/vnib.dir/vnib/vnib/vnib.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "BUILD_IB"
+  "USE_64BITS"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  ".././ibapi/linux/client"
+  "../IB_PATH"
+  "/usr/include/python2.7"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
